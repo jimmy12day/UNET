7aa12142d70eab51fea36c42b91f6ce5c6977962
Bin <jimmy28year@gmail.com>
2019-10-22 21:57:30 -0700

    Basic network environment setup

---
+++ Assets/Scripts/PlayerConnection.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+
+public class PlayerConnection : NetworkBehaviour
+{
+    [SerializeField]
+    GameObject playerUnit;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isLocalPlayer == false)
+        {
+            return;
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            CmdSpawnPlayer();
+        }
+
+    }
+
+    [Command]
+    void CmdSpawnPlayer()
+    {
+        if (playerUnit == null)
+            return;
+        GameObject playerprefab = Instantiate(playerUnit);
+        NetworkServer.Spawn(playerUnit);
+    }
+}
